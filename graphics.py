14834ce606fceaa2d7f5ff054ffa1e6d87a6c443
AL-1S-art <090410pts@gmail.com>
2026-06-27 18:06:05 +0900

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -9,6 +9,9 @@ screen = pygame.display.set_mode([1920, 1080])
 # 장면
 scene = 'intro'
 # scene = 'loading'
+effect = 'nothing'
+
+
 
 # confirm_program_quit
 y_confirm_program_quit = 490
@@ -28,16 +31,19 @@ is_mouse_down = False
 #글씨 및 이미지 부르기
 base_path = os.path.dirname(__file__)
 smallpen = pygame.font.SysFont("malgungothic", 30, bold=True, italic=False)
-original = pygame.font.SysFont("malgungothic", 50, bold=True, italic=False)
-title = pygame.font.SysFont("malgungothic", 100, bold=True, italic=False)
+between_sn = pygame.font.SysFont("malgungothic", 40, bold=True, italic=False)
+normalpen = pygame.font.SysFont("malgungothic", 50, bold=True, italic=False)
+between_nb = pygame.font.SysFont("malgungothic", 75, bold=True, italic=False)
+bigpen = pygame.font.SysFont("malgungothic", 100, bold=True, italic=False)
 backtitle = pygame.image.load(os.path.join(base_path, "Graphics/backtitle.png"))
 backtitle = pygame.transform.scale(backtitle, (1920, 1080))
 black = pygame.image.load(os.path.join(base_path, "Graphics/black.png"))
 black = pygame.transform.scale(black, (1920, 1080))
+modeback = pygame.image.load(os.path.join(base_path, "Graphics/modeback.png"))
+modeback = pygame.transform.scale(modeback, (1920, 1080))
+bookback = pygame.image.load(os.path.join(base_path, "Graphics/bookback.png"))
+bookback = pygame.transform.scale(bookback, (1700, 990))
 # image2 = pygame.image.load("제목 없음.png")
-
-
-
 
 
 
@@ -51,7 +57,7 @@ while running:
     for event in pygame.event.get():
         if event.type == 768:
             if event.key == 27:  # 27 : ESC key
-                scene = "confirm_program_quit"
+                effect = "confirm_program_quit"
 
             elif event.key == 13:  # enter
                 is_enter_down = True
@@ -123,7 +129,7 @@ while running:
 
 
     # 이벤트로 인한 변화 처리(물리 이동, 공격 hp 감소라든지...)
-    if scene == "confirm_program_quit":
+    if effect == "confirm_program_quit":
         if is_top_down == True:
             y_confirm_program_quit = max(490, y_confirm_program_quit - 100)
             is_top_down = False
@@ -136,14 +142,14 @@ while running:
             if y_confirm_program_quit == 490:
                 running = False
             elif y_confirm_program_quit == 590:
-                scene = "intro"
+                effect = "nothing"
             is_enter_down = False
 
         if is_mouse_down == True:
             if y_confirm_program_quit == 490:
                 running = False
             elif y_confirm_program_quit == 590:
-                scene = "intro"
+                effect = "nothing"
             is_mouse_down = False
 
         if is_mouse_move == True:
@@ -209,13 +215,10 @@ while running:
 
 
 
+#배경
 
-
-
-
-    # 그리기
     screen.blit(backtitle, (0, 0))
-    titlename = title.render("세계 제일 직업 대회", True, (0, 0, 0))
+    titlename = bigpen.render("세계 제일 직업 대회", True, (0, 0, 0))
     pygame.draw.rect(screen, (255, 0, 0), (434.5, 75, 1055, 184))
     screen.blit(titlename, (509.5, 100))
     press_start = smallpen.render("PRESS BUTTON", True, (255, 255, 255))
@@ -224,24 +227,12 @@ while running:
 
 
     
-    if scene == "confirm_program_quit":
-        pygame.draw.rect(screen, (0, 0, 0), (450, 225, 1020, 550))
-        pygame.draw.rect(screen, (255, 0, 0), (537.5, y_confirm_program_quit, 845, 87))
-        realend = title.render("종료하시겠습니까?", True, (255, 255, 255))
-        yes = original.render("예", True, (255, 255, 255))
-        no = original.render("아니요", True, (255, 255, 255))
-        screen.blit(realend, (537.5, 300))
-        screen.blit(yes, (935, 500))
-        screen.blit(no, (885, 600))
-
-
-
 
     if scene == "loading":
         time.sleep(0.25)
         screen.blit(black, (0, 0))
         pygame.display.flip()
-        for i in range(3):
+        for i in range(1):
             pygame.display.flip()
             loading_ment = smallpen.render("다양한 직업들 모으는 중.", True, (255, 255, 255))
             screen.blit(loading_ment, (789.5, 800))
@@ -264,21 +255,26 @@ while running:
 
 
     if scene == "mode_pick":
-        modePVP = original.render("PVP", True, (255, 255, 255))
-        modeBOSS = original.render("제빵사", True, (255, 255, 255))
+        modePVP = normalpen.render("PVP", True, (255, 255, 255))
+        modeBOSS = normalpen.render("제빵사", True, (255, 255, 255))
         screen.blit(blackdeath, (789.5, 800))
         screen.blit(blackdeath, (789.5, 800))
-
-
-
-
-
-
-
 
 
     if scene == "character_pick":
-        # 210 + 280 * (n - 1)
+        # 가로 360 + 280 * (n - 1)
+        # 세로 90 + 300 * (n - 1)
+        # 모든 smallpen 글씨는 세로 40 차지
+        # 세로 중 차지하는 정도: 그림 250, 글씨 50
+        # 가로길이, 140-(가로길이/2)
+        screen.blit(modeback, (0, 0))
+        screen.blit(bookback, (110, 45))
+        pygame.draw.rect(screen, (255, 0, 0), (310, 45, 50, 990))
+        pygame.draw.rect(screen, (255, 0, 0), (1760, 45, 50, 990))
+        character_li = between_nb.render("직업", True, (255, 255, 255))
+        character_st = between_nb.render("사전", True, (255, 255, 255))
+        screen.blit(character_li, (135, 435))
+        screen.blit(character_st, (135, 545))
         fighter = smallpen.render("격투가", True, (255, 255, 255))
         gambler = smallpen.render("도박꾼", True, (255, 255, 255))
         rider = smallpen.render("라이더", True, (255, 255, 255))
@@ -289,32 +285,48 @@ while running:
         naturalist = smallpen.render("자연술사", True, (255, 255, 255))
         politician = smallpen.render("정치인", True, (255, 255, 255))
         baker = smallpen.render("제빵사", True, (255, 255, 255))
-        chessplayer = smallpen.render("체스 선수", True, (255, 255, 255))
+        chessplayer = smallpen.render("체스선수", True, (255, 255, 255))
         pitcher = smallpen.render("투수", True, (255, 255, 255))
         harrypotter = smallpen.render("해리포터 3회 독자", True, (255, 255, 255))
         chemist = smallpen.render("화학자", True, (255, 255, 255))
         blackdeath = smallpen.render("흑사병 보균자", True, (255, 255, 255))
-        screen.blit(fighter, (304.5, 800))
-        screen.blit(gambler, (304.5, 800))
-        screen.blit(rider, (304.5, 800))
-        screen.blit(carpenter, (304.5, 800))
-        screen.blit(bodybuilder, (304.5, 800))
-        screen.blit(engineer, (304.5, 800))
-        screen.blit(musician, (304.5, 800))
-        screen.blit(naturalist, (304.5, 800))
-        screen.blit(politician, (304.5, 800))
-        screen.blit(baker, (304.5, 800))
-        screen.blit(chessplayer, (304.5, 800))
-        screen.blit(pitcher, (304.5, 800))
-        screen.blit(harrypotter, (505.5, 800))
-        screen.blit(chemist, (304.5, 800))
-        screen.blit(blackdeath, (304.5, 800))
+
+        screen.blit(fighter, (455, 345)) # 90, 95
+        screen.blit(gambler, (735, 345)) # 90, 95
+        screen.blit(rider, (1015, 345)) # 90, 95
+        screen.blit(carpenter, (1310, 345)) # 60, 110
+        screen.blit(bodybuilder, (1560, 345)) # 120, 80
+
+        screen.blit(engineer, (440, 645)) # 120, 80
+        screen.blit(musician, (735, 645)) # 90, 95
+        screen.blit(naturalist, (1000, 645)) # 120, 80
+        screen.blit(politician, (1295, 645)) # 90, 95
+        screen.blit(baker, (1575, 645)) # 90, 95
+
+        screen.blit(chessplayer, (440, 945)) # 120, 80
+        screen.blit(pitcher, (750, 945)) # 60, 110
+        screen.blit(harrypotter, (935.5, 945)) # 249, 15.5
+        screen.blit(chemist, (1295, 945)) # 90, 95
+        screen.blit(blackdeath, (1524.5, 945)) # 191, 44.5
 
 
 
 
 
 
+
+
+
+#위에 최종적으로 걸쳐지는 것
+    if effect == "confirm_program_quit":
+        pygame.draw.rect(screen, (0, 0, 0), (450, 225, 1020, 550))
+        pygame.draw.rect(screen, (255, 0, 0), (537.5, y_confirm_program_quit, 845, 87))
+        realend = bigpen.render("종료하시겠습니까?", True, (255, 255, 255))
+        yes = normalpen.render("예", True, (255, 255, 255))
+        no = normalpen.render("아니요", True, (255, 255, 255))
+        screen.blit(realend, (537.5, 300))
+        screen.blit(yes, (935, 500))
+        screen.blit(no, (885, 600))
 
 
     pygame.display.flip()
@@ -322,9 +334,8 @@ while running:
 
 pygame.quit()
 
-width = harrypotter.get_width()
-height = harrypotter.get_height()
-
+width = character_list.get_width()
+height = character_list.get_height()
 print(width, height)
 
 
@@ -336,20 +347,3 @@ print(width, height)
 # 엔터키 13
 # 스페이스바 32
 # ESC키 27
-
-    #화면에 글씨 띄울 글씨 및 이미지 설정
-    # pygame.draw.rect(screen, (255, 0, 0), (x, y, 200, 30))
-    # title = title.render("세계 제일 직업 대회", True, (255, 255, 255))
-
-    # text1 = original.render("격투가", True, (255, 255, 255))
-    # text1 = original.render("격투가", True, (255, 255, 255))
-    # text2 = original.render("도박꾼", True, (255, 255, 255))
-    # text3 = original.render("자연술사", True, (255, 255, 255))
-    # text4 = original.render("흑사병 보균자", True, (255, 255, 255))
-    # screen.blit(title, (50, 50))
-    # screen.blit(text1, (100, 200))
-    # screen.blit(text2, (100, 300))
-    # screen.blit(text3, (100, 400))
-    # screen.blit(text4, (100, 500))
-    # screen.blit(image, (x, y))
-    # screen.blit(image2, (0, 0))
