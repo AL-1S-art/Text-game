9f95292f4edf506c8dddea274882d8681b2ffa92
AL-1S-art <090410pts@gmail.com>
2026-07-11 18:00:55 +0900

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -6,13 +6,22 @@ import os
 pygame.font.init()
 screen = pygame.display.set_mode([1920, 1080]) 
 
+
+#화면 어둡게
+fade = pygame.Surface((1920, 1080))
+fade.fill((0, 0, 0))
+
+
+
 # 장면
 scene = 'intro'
-
+clock = pygame.time.Clock()
 
 # confirm_program_quit
 y_confirm_program_quit = 490
-x_mode_pick = 235
+x_main_pick = 235
+x_game_mode_pick = 360
+x_story_mode_pick = 360
 
 # 키 입력, 마우스 입력 이벤트 처리
 key_down_manager = {chr(i): False for i in range(97, 122 + 1)}
@@ -39,34 +48,31 @@ bigpen = pygame.font.SysFont("malgungothic", 100, bold=True, italic=False)
 
 backtitle = pygame.image.load(os.path.join(base_path, "Graphics/backtitle.png"))
 backtitle = pygame.transform.scale(backtitle, (1920, 1080))
-
 black = pygame.image.load(os.path.join(base_path, "Graphics/black.png"))
 black = pygame.transform.scale(black, (1920, 1080))
-
 mainback = pygame.image.load(os.path.join(base_path, "Graphics/mainback.png"))
 mainback = pygame.transform.scale(mainback, (1920, 1080))
-
 bookback = pygame.image.load(os.path.join(base_path, "Graphics/bookback.png"))
 bookback = pygame.transform.scale(bookback, (1700, 990))
-
 option_icon1 = pygame.image.load(os.path.join(base_path, "Graphics/option_icon1.png"))
 option_icon1 = pygame.transform.scale(option_icon1, (150, 150))
-
 home_settingback = pygame.image.load(os.path.join(base_path, "Graphics/home_settingback.png"))
 home_settingback = pygame.transform.scale(home_settingback, (1920, 1080))
-
 modeback = pygame.image.load(os.path.join(base_path, "Graphics/modeback.png"))
 modeback = pygame.transform.scale(modeback, (1920, 1080))
-
 endback = pygame.image.load(os.path.join(base_path, "Graphics/endback.png"))
 endback = pygame.transform.scale(endback, (1920, 1080))
+
+
+Naturalist = pygame.image.load(os.path.join(base_path, "Graphics/캐릭터 디자인/design_Naturalist.png"))
+Naturalist = pygame.transform.scale(Naturalist, (280, 250))
 # image2 = pygame.image.load("제목 없음.png")
 
 
 
 
-
-
+fade_alpha = 255
+fade_done = False
 #반복해서 창 실행
 running = True
 while running:
@@ -142,11 +148,11 @@ while running:
 
     if scene == "main":
         if is_right_down == True:
-            x_mode_pick = min(1285, x_mode_pick + 525)
+            x_main_pick = min(1285, x_main_pick + 525)
             is_right_down = False
 
         if is_left_down == True:
-            x_mode_pick = max(235, x_mode_pick - 525)
+            x_main_pick = max(235, x_main_pick - 525)
             is_left_down = False
 
         if is_mouse_move == True:
@@ -160,11 +166,11 @@ while running:
             is_mouse_move = False
 
         if is_enter_down == True:
-            if x_mode_pick == 235:
+            if x_main_pick == 235:
                 scene = "game_mode_pick"
-            elif x_mode_pick == 760:
-                scene = "story_pick"
-            elif x_mode_pick == 1285:
+            elif x_main_pick == 760:
+                scene = "story_mode_pick"
+            elif x_main_pick == 1285:
                 scene = "character_pick"
             is_enter_down = False
 
@@ -174,7 +180,7 @@ while running:
                 if 235 <= mouse_x and mouse_x <= 635:
                     scene = "game_mode_pick"
                 if 760 <= mouse_x and mouse_x <= 1160:
-                    scene = "story_pick"
+                    scene = "story_mode_pick"
                 if 1285 <= mouse_x and mouse_x <= 1685:
                     scene = "character_pick"
             if mouse_y <= 150:
@@ -254,23 +260,97 @@ while running:
 
 
 
-    if scene == "story_pick":
+    if scene == "story_mode_pick":
+        if is_right_down == True:
+            x_story_mode_pick = min(1060, x_story_mode_pick + 700)
+            is_right_down = False
+
+        if is_left_down == True:
+            x_story_mode_pick = max(360, x_story_mode_pick - 700)
+            is_left_down = False
+
+        if is_mouse_move == True:
+            if mouse_y >= 212.5 and mouse_y <= 1017.5:
+                if 360 <= mouse_x and mouse_x <= 860:
+                    x_mode_pick = 360
+                if 1060 <= mouse_x and mouse_x <= 1560:
+                    x_mode_pick = 1060
+            is_mouse_move = False
+
+
+        if is_enter_down == True:
+            if x_story_mode_pick == 360:
+                scene = ""
+            elif x_story_mode_pick == 1060:
+                scene = ""
+            is_enter_down = False
+
+
+        # if is_mouse_down == True:
+        #     if mouse_y >= 212.5 and mouse_y <= 1017.5:
+        #         if 235 <= mouse_x and mouse_x <= 635:
+        #             scene = "game_mode_pick"
+        #         if 760 <= mouse_x and mouse_x <= 1160:
+        #             scene = "story_mode_pick"
+        #         if 1285 <= mouse_x and mouse_x <= 1685:
+        #             scene = "character_pick"
+        #     if mouse_y <= 150:
+        #         if mouse_x >= 1770:
+        #             scene = "home_setting"
+        #     is_mouse_down = False
+
+
+
+
         if is_esc_down == True:
             scene = "main"
             is_esc_down = False
+
+
 
 
     if scene == "game_mode_pick":
+        if is_right_down == True:
+            x_game_mode_pick = min(1060, x_game_mode_pick + 700)
+            is_right_down = False
+
+        if is_left_down == True:
+            x_game_mode_pick = max(360, x_game_mode_pick - 700)
+            is_left_down = False
+
+        if is_mouse_move == True:
+            if mouse_y >= 212.5 and mouse_y <= 1017.5:
+                if 360 <= mouse_x and mouse_x <= 860:
+                    x_game_mode_pick = 360
+                if 1060 <= mouse_x and mouse_x <= 1560:
+                    x_game_mode_pick = 1060
+            is_mouse_move = False
+
+
+        if is_enter_down == True:
+            if x_game_mode_pick == 360:
+                scene = ""
+            elif x_game_mode_pick == 1060:
+                scene = ""
+            is_enter_down = False
+
+
+        # if is_mouse_down == True:
+        #     if mouse_y >= 212.5 and mouse_y <= 1017.5:
+        #         if 235 <= mouse_x and mouse_x <= 635:
+        #             scene = "game_mode_pick"
+        #         if 760 <= mouse_x and mouse_x <= 1160:
+        #             scene = "story_mode_pick"
+        #         if 1285 <= mouse_x and mouse_x <= 1685:
+        #             scene = "character_pick"
+        #     if mouse_y <= 150:
+        #         if mouse_x >= 1770:
+        #             scene = "home_setting"
+        #     is_mouse_down = False
+
         if is_esc_down == True:
             scene = "main"
             is_esc_down = False
-
-
-
-
-
-
-
 
 
 
@@ -293,6 +373,8 @@ while running:
     pygame.draw.rect(screen, (0, 0, 0), (850, 805, 220, 30))
     screen.blit(press_start, (850, 800))
 
+
+
     
     if scene == "confirm_program_quit":
         screen.blit(endback, (0, 0))
@@ -307,12 +389,14 @@ while running:
 
 
 
+
     if scene == "home_setting":
         screen.blit(home_settingback, (0, 0))
         quit = between_nb.render("게임 종료", True, (255, 255, 255))
         opt = bigpen.render("설정", True, (255, 255, 255))
         screen.blit(quit, (797, 900))
         screen.blit(opt, (860, 50))
+
 
 
     if scene == "main":
@@ -323,7 +407,7 @@ while running:
         pygame.draw.rect(screen, (0, 0, 0), (1285, 212.5, 400, 805))
         screen.blit(option_icon1, (1770, 0))
         titlename = bigpen.render("세계 제일 직업 대회", True, (255, 255, 255))
-        pygame.draw.rect(screen, (255, 0, 0), (x_mode_pick, 212.5, 400, 805))
+        pygame.draw.rect(screen, (255, 0, 0), (x_main_pick, 212.5, 400, 805))
         mainGAME1 = bigpen.render("게임", True, (255, 255, 255))
         mainGAME2 = bigpen.render("시작", True, (255, 255, 255))
         mainSTORY = bigpen.render("스토리", True, (255, 255, 255))
@@ -361,39 +445,41 @@ while running:
         character_st = between_nb.render("사전", True, (255, 255, 255))
         screen.blit(character_li, (135, 435))
         screen.blit(character_st, (135, 545))
-        fighter = smallpen.render("격투가", True, (255, 255, 255))
-        gambler = smallpen.render("도박꾼", True, (255, 255, 255))
-        rider = smallpen.render("라이더", True, (255, 255, 255))
-        carpenter = smallpen.render("목수", True, (255, 255, 255))
-        bodybuilder = smallpen.render("보디빌더", True, (255, 255, 255))
-        engineer = smallpen.render("엔지니어", True, (255, 255, 255))
-        musician = smallpen.render("음악가", True, (255, 255, 255))
-        naturalist = smallpen.render("자연술사", True, (255, 255, 255))
-        politician = smallpen.render("정치인", True, (255, 255, 255))
-        baker = smallpen.render("제빵사", True, (255, 255, 255))
-        chessplayer = smallpen.render("체스선수", True, (255, 255, 255))
-        pitcher = smallpen.render("투수", True, (255, 255, 255))
-        harrypotter = smallpen.render("해리포터 3회 독자", True, (255, 255, 255))
-        chemist = smallpen.render("화학자", True, (255, 255, 255))
-        blackdeath = smallpen.render("흑사병 보균자", True, (255, 255, 255))
+        fighter_name = smallpen.render("격투가", True, (255, 255, 255))
+        gambler_name = smallpen.render("도박꾼", True, (255, 255, 255))
+        rider_name = smallpen.render("라이더", True, (255, 255, 255))
+        carpenter_name = smallpen.render("목수", True, (255, 255, 255))
+        bodybuilder_name = smallpen.render("보디빌더", True, (255, 255, 255))
+        engineer_name = smallpen.render("엔지니어", True, (255, 255, 255))
+        musician_name = smallpen.render("음악가", True, (255, 255, 255))
+        naturalist_name = smallpen.render("자연술사", True, (255, 255, 255))
+        politician_name = smallpen.render("정치인", True, (255, 255, 255))
+        baker_name = smallpen.render("제빵사", True, (255, 255, 255))
+        chessplayer_name = smallpen.render("체스선수", True, (255, 255, 255))
+        pitcher_name = smallpen.render("투수", True, (255, 255, 255))
+        harrypotter_name = smallpen.render("해리포터 3회 독자", True, (255, 255, 255))
+        chemist_name = smallpen.render("화학자", True, (255, 255, 255))
+        blackdeath_name = smallpen.render("흑사병 보균자", True, (255, 255, 255))
 
-        screen.blit(fighter, (455, 345)) # 90, 95
-        screen.blit(gambler, (735, 345)) # 90, 95
-        screen.blit(rider, (1015, 345)) # 90, 95
-        screen.blit(carpenter, (1310, 345)) # 60, 110
-        screen.blit(bodybuilder, (1560, 345)) # 120, 80
+        screen.blit(fighter_name, (455, 345)) # 90, 95
+        screen.blit(gambler_name, (735, 345)) # 90, 95
+        screen.blit(rider_name, (1015, 345)) # 90, 95
+        screen.blit(carpenter_name, (1310, 345)) # 60, 110
+        screen.blit(bodybuilder_name, (1560, 345)) # 120, 80
 
-        screen.blit(engineer, (440, 645)) # 120, 80
-        screen.blit(musician, (735, 645)) # 90, 95
-        screen.blit(naturalist, (1000, 645)) # 120, 80
-        screen.blit(politician, (1295, 645)) # 90, 95
-        screen.blit(baker, (1575, 645)) # 90, 95
+        screen.blit(engineer_name, (440, 645)) # 120, 80
+        screen.blit(musician_name, (735, 645)) # 90, 95
+        screen.blit(naturalist_name, (1000, 645)) # 120, 80
+        screen.blit(Naturalist, (920, 385))
+        screen.blit(politician_name, (1295, 645)) # 90, 95
+        screen.blit(baker_name, (1575, 645)) # 90, 95
 
-        screen.blit(chessplayer, (440, 945)) # 120, 80
-        screen.blit(pitcher, (750, 945)) # 60, 110
-        screen.blit(harrypotter, (935.5, 945)) # 249, 15.5
-        screen.blit(chemist, (1295, 945)) # 90, 95
-        screen.blit(blackdeath, (1524.5, 945)) # 191, 44.5
+        screen.blit(chessplayer_name, (440, 945)) # 120, 80
+        screen.blit(pitcher_name, (750, 945)) # 60, 110
+        screen.blit(harrypotter_name, (935.5, 945)) # 249, 15.5
+        screen.blit(chemist_name, (1295, 945)) # 90, 95
+        screen.blit(blackdeath_name, (1524.5, 945)) # 191, 44.5
+
 
 
 
@@ -402,17 +488,21 @@ while running:
         screen.blit(modeback, (0, 0))
         pygame.draw.rect(screen, (0, 0, 0), (360, 90, 500, 900))
         pygame.draw.rect(screen, (0, 0, 0), (1060, 90, 500, 900))
+        pygame.draw.rect(screen, (255, 0, 0), (x_game_mode_pick, 212.5, 400, 805))
         gamePVP = bigpen.render("PVP", True, (255, 255, 255))
         gameBOSS = bigpen.render("BOSS", True, (255, 255, 255))
         screen.blit(gamePVP, (515.5, 473))
         screen.blit(gameBOSS, (1180, 473))
 
 
-    if scene == "story_pick":
+
+
+    if scene == "story_mode_pick":
         screen.blit(modeback, (0, 0))
         screen.blit(modeback, (0, 0))
         pygame.draw.rect(screen, (0, 0, 0), (360, 90, 500, 900))
         pygame.draw.rect(screen, (0, 0, 0), (1060, 90, 500, 900))
+        pygame.draw.rect(screen, (255, 0, 0), (x_story_mode, 212.5, 400, 805))
         storyMAIN1 = bigpen.render("메인", True, (255, 255, 255))
         storyMAIN2 = bigpen.render("스토리", True, (255, 255, 255))
         storySIDE1 = bigpen.render("사이드", True, (255, 255, 255))
@@ -421,6 +511,8 @@ while running:
         screen.blit(storyMAIN2, (460, 540))
         screen.blit(storySIDE1, (1160, 406))
         screen.blit(storySIDE2, (1160, 540))
+        
+
 
 
 
@@ -434,9 +526,8 @@ while running:
 
 
     if scene == "loading_to_main":
-        time.sleep(0.25)
+        clock.tick(60)
         screen.blit(black, (0, 0))
-        pygame.display.flip()
         for i in range(1):
             pygame.display.flip()
             loading_ment = smallpen.render("다양한 직업들 모으는 중.", True, (255, 255, 255))
@@ -454,21 +545,27 @@ while running:
             pygame.display.flip()
             time.sleep(0.5)
             screen.blit(black, (0, 0))
-            loading_ment = smallpen.render("다양한 직업들 모으는 중.", True, (255, 255, 255))
-            screen.blit(loading_ment, (789.5, 800))
+        scene = "main"
+        pygame.event.clear()
         is_left_down = False
         is_right_down = False
-        is_top_down = False
-        is_bottom_down = False
         is_enter_down = False
-        is_space_down = False
         is_esc_down = False
-        is_mouse_move = False
-        is_mouse_down = False
-        scene = "main"
+
+        
+    if not fade_done:
+            fade_alpha -= 5
+
+    if fade_alpha <= 0:
+        fade_alpha = 0
+        fade_done = True
+
+    fade.set_alpha(fade_alpha)
+    screen.blit(fade, (0, 0))
 
 
     pygame.display.flip()
+    clock.tick(60)
 
 
 pygame.quit()
@@ -476,15 +573,7 @@ pygame.quit()
 width = storyMAIN1.get_width()
 height = storyMAIN1.get_height()
 print(width, height)
-width = storyMAIN2.get_width()
-height = storyMAIN2.get_height()
-print(width, height)
-width = storySIDE1.get_width()
-height = storySIDE1.get_height()
-print(width, height)
-width = storySIDE2.get_width()
-height = storySIDE2.get_height()
-print(width, height)
+
 
 # 누를때 768 땔때 769인 키들
 # 윗키 1073741906
@@ -497,5 +586,5 @@ print(width, height)
 
 
 # scene
-# loading_to_mode   시작화면에서 메인화면으로
+# loading_to_main   시작화면에서 메인화면으로
 # home_setting, main, character_pick
