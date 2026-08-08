a010962937f44702be81dad3ccd32be3f75a57f0
AL-1S-art <090410pts@gmail.com>
2026-08-08 17:32:46 +0900

    Update main_story.py

--- component_prac/Scenes/main_story.py
+++ component_prac/Scenes/main_story.py
@@ -2,35 +2,45 @@ import pygame
 from Components.button import Button
 import os
 
-class Main_story:
+class Main:
     def __init__(self, screen):
         self.screen = screen
         self.next_scene = None
-        self.menu = Button(self.screen, 0, 0, 1920, 150, (0,0,0), "스토리", "b", (255,255,255),"n")
-        self.storyMAIN = Button(self.screen, 360, 215, 500, 800, (0,0,0), "메인", "b", (255,255,255),"y")
-        self.storySIDE = Button(self.screen, 1060, 215, 500, 800, (0,0,0), "사이드", "b", (255,255,255),"y")
+        self.menu = Button(self.screen, 0, 0, 1920, 150, (0,0,0), "세계 제일 직업 대회", "b", (255,255,255),"n")
+        self.game = Button(self.screen, 235, 212.5, 400, 805, (0,0,0), "시작", "b", (255,255,255),"y")
+        self.story = Button(self.screen, 760, 212.5, 400, 805, (0,0,0), "스토리", "b", (255,255,255),"y")
+        self.character = Button(self.screen, 1285, 212.5, 400, 805, (0,0,0), "캐릭터", "b", (255,255,255),"y")
         self.base_path = os.path.dirname(__file__)
-        self.characterback = pygame.image.load(os.path.join(self.base_path, "Graphics/scene_mode/modeback.png"))
-        self.characterback = pygame.transform.scale(self.characterback, (1920, 1080))
+        self.mainback = pygame.image.load(os.path.join(self.base_path, "Graphics/scene_main/mainback.png"))
+        self.mainback = pygame.transform.scale(self.mainback, (1920, 1080))
 
 
     def handle_event(self, event: pygame.event):
         if event.type == 768:
             if event.key == 27:
-                self.next_scene = 'main'
+                self.next_scene = "home_setting"
+            
+        if self.game.handle_event(event) == "next":
+            self.next_scene = "game_mode_pick"
 
-        if self.storyMAIN.handle_event(event) == "next":
-            self.next_scene = "main_story"
+        if self.story.handle_event(event) == "next":
+            self.next_scene = "story_mode_pick"
+            
+        if self.character.handle_event(event) == "next":
+            self.next_scene = "character_dict"
 
-        if self.storySIDE.handle_event(event) == "next":
-            self.next_scene = "side_story"
+        
     
             
     def update(self):
-        self.storyMAIN.update()
-        self.storySIDE.update()
+        self.menu.update()
+        self.game.update()
+        self.story.update()
+        self.character.update()
 
     def draw(self):
-        self.screen.blit(self.characterback, (0,0))
-        self.storyMAIN.draw()
-        self.storySIDE.draw()
+        self.screen.blit(self.mainback, (0,0))
+        self.menu.draw()
+        self.game.draw()
+        self.story.draw()
+        self.character.draw()
