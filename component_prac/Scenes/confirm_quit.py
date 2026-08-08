d6b2c7cd2bb9370259a6a0a33d79175f589949ff
AL-1S-art <090410pts@gmail.com>
2026-08-08 17:31:47 +0900

    Update confirm_quit.py

--- component_prac/Scenes/confirm_quit.py
+++ component_prac/Scenes/confirm_quit.py
@@ -6,9 +6,9 @@ class Confirm_quit:
     def __init__(self, screen):
         self.screen = screen
         self.next_scene = None
-        self.real_quit = Button(self.screen, 0, 0, 1920, 150, (0,0,0), "종료하시겠습니까?", "b", (255,255,255),"n")
-        self.yes = Button(self.screen, 537.5, 490, 845, 87, (0,0,0), "예", "b", (255,255,255),"y")
-        self.no = Button(self.screen, 537.5, 590, 845, 87, (0,0,0), "아니요", "b", (255,255,255),"y")
+        self.real_quit = Button(self.screen, 450, 225, 1020, 300, (0,0,0), "종료하시겠습니까?", "b", (255,255,255),"n")
+        self.yes = Button(self.screen, 537.5, 490, 845, 87, (0,0,0), "예", "n", (255,255,255),"y")
+        self.no = Button(self.screen, 537.5, 590, 845, 87, (0,0,0), "아니요", "n", (255,255,255),"y")
         self.base_path = os.path.dirname(__file__)
         self.endback = pygame.image.load(os.path.join(self.base_path, "Graphics/scene_confirm_quit/endback.png"))
         self.endback = pygame.transform.scale(self.endback, (1920, 1080))
